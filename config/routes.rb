b5c827868645793d8d988c7a949277ac20a9739d
Lutriel Dangleben <lutrieldangleben@gmail.com>
2018-08-14 21:17:54 +0000

    messages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :mains
    get 'mains' => 'mains#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+     
+  resources :help
+    get '/:page' => 'help#index'
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
