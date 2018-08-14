52ebbd6b584ef8d146d3ef308b13800178dd447e
Joshua Meadows <joshuakmeadows@gmail.com>
2018-08-14 22:16:13 +0000

    fixed routes again

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  resources :movies
- get 'home/index'
+  get 'home/index'
 
  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
  resources :mains
@@ -16,4 +16,3 @@ authenticated :user do
 
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
