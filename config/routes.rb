664feed34e1ecb6a51b35c2623efe48dc09f44aa
Joshua Meadows <joshuakmeadows@gmail.com>
2018-08-14 21:15:33 +0000

    Half way through faceboot OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :users
   resources :mains
    get 'mains' => 'mains#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root 'home#index', as: 'authenticated_root'
+  end
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
 end
