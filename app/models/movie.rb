9021fa6478a4ed9e18dbf23e38d16e664aafeccd
Lutriel Dangleben <lutrieldangleben@gmail.com>
2018-08-14 23:33:25 +0000

    update4

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ApplicationRecord
-    has_attached_file :poster, styles: { medium:  '400x600#'}
+    has_attached_file :poster, styles: { medium:  '400x400#'}
      validates_attachment_content_type :poster, content_type: /\Aimage\/.*\Z/
 end
