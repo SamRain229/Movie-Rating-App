4b56e61e1569b581c67f09bb96f09c0a8d96c314
Joshua Meadows <joshuakmeadows@gmail.com>
2018-08-15 23:47:07 +0000

    AAAA

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,12 @@
 
 ActiveRecord::Schema.define(version: 20180815193444) do
 
+  create_table "mains", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.text "description"
