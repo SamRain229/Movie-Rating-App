073959368aa4989520f17131b307b3cf8e273b07
Lutriel Dangleben <lutrieldangleben@gmail.com>
2018-08-15 23:22:40 +0000

    update 5

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,4 @@ authenticated :user do
  end
 resources :authentications, only: [:destroy]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
