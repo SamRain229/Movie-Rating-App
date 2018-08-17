458845b307aca37475ad3bf74cd60eaa68987ac9
SamRain229 <joshuakmeadows@gmail.com>
2018-08-17 17:42:27 -0400

    fixed active record issue

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,10 @@
 class Movie < ApplicationRecord
-   belongs_to rating
-   belongs_to description
-   belongs_to title
-   belongs_to director
-   belongs_to movie_length
-   belongs_to commit
+   belongs_to :rating
+   belongs_to :description
+   belongs_to :title
+   belongs_to :director
+   belongs_to :movie_length
+   belongs_to :commit
 
    has_attached_file :poster, styles: { medium:  '400x400#'}
     validates_attachment_content_type :poster, content_type: /\Aimage\/.*\Z/
