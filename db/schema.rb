9aefed1fbe52721ca756f85fd2f76c09ee02175f
Lutriel Dangleben <lutrieldangleben@gmail.com>
2018-08-18 22:28:46 +0000

    h

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180816215225) do
+ActiveRecord::Schema.define(version: 20180817031209) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
